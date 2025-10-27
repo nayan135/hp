11aec65997c6f5d7751bb312e83d04a3d17ee447
NAYAN ACHARYA <100992818+nayan135@users.noreply.github.com>
2025-10-27 19:17:13 +0545

    i think this is it

--- hooks/audio.ts
+++ hooks/audio.ts
@@ -1,15 +1,41 @@
-export function audio(audioUrl: string, autoPlay= false){
-    const audioR = useRef<HTMLAudioElement | null>(null);
+import { useEffect, useRef } from "react";
 
-    useEffect(()=> {
-        audioR.current = new Audio(audioUrl);
+export function audio(audioUrl: string, autoPlay= false){
+    const audioR = useRef<HTMLAudioElement | null>(null)
+
+    useEffect(() => {
+          const audio = new Audio(audioUrl)
+        audioR.current = audio
 
         if(autoPlay){
-            audioR.current.play().catch((error) => {
-                console.error("Error playing audio:", error);
-        }
+            audio.play().catch((error) => {
+                console.error("Error playing audio:", error)
+        })
 
+        }
+    return() => {
+        audio.pause()
+        audio.currentTime = 0
+    }
+
+},[audioUrl, autoPlay])
+
+const play = () => {
+    if(audioR.current){
+        audioR.current.currentTime =0
+        audioR.current.play().catch((error) => {
+            console.error("Error playing audio:", error)
     })
-    return
+}
 
 }
+const stop = () => {
+    if(audioR.current){
+        audioR.current.pause()
+        audioR.current.currentTime =0
+    }
+  }
+
+  return { play, stop, audioR }
+}
+
